37310fbf198b5a477ea323e6d5c131cd1debd5e2
Peter Bryzgalov <peterbryz@yahoo.com>
2017-11-09 23:19:26 +0900

    Added HPCG MacBook Pro logs.

--- Tensorflow-HP/run_benchmarks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-apt-get update && apt-get install -y git
-pip install -U tf-nightly
-cd /root
-git clone https://github.com/tensorflow/benchmarks.git
-cd benchmarks/scripts/tf_cnn_benchmarks/
-pwd && ls -l
-set -x
-python tf_cnn_benchmarks.py  --batch_size=32 --model=resnet50 --variable_update=parameter_server
