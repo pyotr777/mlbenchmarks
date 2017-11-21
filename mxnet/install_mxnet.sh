7e064cd8a10134e121348240c909339383e3a17d
Peter Bryzgalov <peterbryz@yahoo.com>
2017-11-21 18:58:03 +0900

    Installation script and MNIST benchmark for MXNet.

---
+++ mxnet/install_mxnet.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# This script will start container with tensorflow/tensorflow:latest-gpu image
+# And run benchmarks in it.
+
+usage=$(cat <<USAGEBLOCK
+Usage:
+$0 [-d <docker command>] [-n/--num_gpus <int>] [--batch_size <int>] [...]
+
+Options:
+	-d					Docker command: docker / nvidia-docker.
+	-n, --num_gpus 		Number of GPUs to use for tests. 0 - use CPU only.
+	-b, --batch_size	Batch size
+	-h, --help			This help info.
+	--debug				Print debug info.
+USAGEBLOCK
+)
+
+
+# Default parameteres
+DOCKER_COMMAND=docker
+NUM_GPUS=0
+BATCH=32
+CONT_NAME=mxnet
+TMP_INSTALL="setup.sh"
+TEST_FILE="test.py"
+TMP_FILE="run_benchmarks.sh"
+
+if [[ $# -lt 1 ]]; then
+	echo "$usage"
+	exit 1
+fi
+
+while test $# -gt 0; do
+	case "$1" in
+		-h | --help)
+			echo "$usage"
+			exit 0
+			;;
+		-d)
+			DOCKER_COMMAND="$2";shift;
+			;;
+		-n | --num_gpus)
+			NUM_GPUS=$2;shift;
+			;;
+		-b | --batch_size)
+			BATCH=$2;shift;
+			;;
+		--debug)
+			debug=YES
+			;;
+		--)
+			shift
+			break;;
+		--*)
+			echo "Unknown option $1"
+			echo "$usage"
+			exit 1
+			;;
+	esac
+	shift
+done
+
+cpu_commands=$(cat <<COMBLOCK1
+import mxnet as mx
+a = mx.nd.ones((2, 3))
+print a
+b = a * 2 + 1
+print b
+b.asnumpy()
+print b
+COMBLOCK1
+)
+
+gpu_commands=$(cat <<COMBLOCK2
+import mxnet as mx
+a = mx.nd.ones((2, 3), mx.gpu())
+print a
+b = a * 2 + 1
+print b
+b.asnumpy()
+print b
+COMBLOCK2
+)
+if [ "$NUM_GPUS" -gt 0 ]; then
+	IMAGE="mxnet/python:gpu"
+	echo "$gpu_command" > $TEST_FILE
+else
+	IMAGE="mxnet/python:latest"
+	echo "$cpu_command" > $TEST_FILE
+fi
+
+if [ -n "$debug" ]; then
+	set -ex
+fi
+
+if [[ -n $(docker ps -q -f "name=$CONT_NAME") ]]; then
+	docker kill $CONT_NAME
+fi
+if [[ -n $(docker ps -a -q -f "name=$CONT_NAME") ]]; then
+	docker rm $CONT_NAME
+fi
+$DOCKER_COMMAND run -td --name $CONT_NAME $IMAGE
+$DOCKER_COMMAND cp $TEST_FILE $CONT_NAME:/root/
+$DOCKER_COMMAND exec -t $CONT_NAME /bin/bash -c /root/$TEST_FILE
+
---
+++ mxnet/mnist.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import mxnet as mx
+from mxnet import nd, autograd
+from mxnet import gluon
+import numpy as np
+data_ctx = mx.cpu()
+model_ctx = mx.cpu()
+# model_ctx = mx.gpu()
+batch_size = 64
+num_inputs = 784
+num_outputs = 10
+num_examples = 60000
+def transform(data, label):
+    return data.astype(np.float32)/255, label.astype(np.float32)
+train_data = mx.gluon.data.DataLoader(mx.gluon.data.vision.MNIST(train=True, transform=transform),
+                                      batch_size, shuffle=True)
+test_data = mx.gluon.data.DataLoader(mx.gluon.data.vision.MNIST(train=False, transform=transform),
+                              batch_size, shuffle=False)
+
+net = gluon.nn.Dense(num_outputs)
+net.collect_params().initialize(mx.init.Normal(sigma=1.), ctx=model_ctx)
+softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.1})
+def evaluate_accuracy(data_iterator, net):
+    acc = mx.metric.Accuracy()
+    for i, (data, label) in enumerate(data_iterator):
+        data = data.as_in_context(model_ctx).reshape((-1,784))
+        label = label.as_in_context(model_ctx)
+        output = net(data)
+        predictions = nd.argmax(output, axis=1)
+        acc.update(preds=predictions, labels=label)
+    return acc.get()[1]
+
+epochs = 3
+moving_loss = 0.
+niter = 0
+
+for e in range(epochs):
+    cumulative_loss = 0
+    for i, (data, label) in enumerate(train_data):
+        data = data.as_in_context(model_ctx).reshape((-1,784))
+        label = label.as_in_context(model_ctx)
+        with autograd.record():
+            output = net(data)
+            loss = softmax_cross_entropy(output, label)
+        loss.backward()
+        trainer.step(batch_size)
+        cumulative_loss += nd.sum(loss).asscalar()
+
+    test_accuracy = evaluate_accuracy(test_data, net)
+    train_accuracy = evaluate_accuracy(train_data, net)
+    print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, cumulative_loss/num_examples, train_accuracy, test_accuracy))
