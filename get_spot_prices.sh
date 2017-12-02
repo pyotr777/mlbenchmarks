d3d68b7e5a30fd4566632e754108ac03d03297e0
Peter Bryzgalov <peterbryz@yahoo.com>
2017-12-03 01:20:42 +0900

    Getting latest spot prices for g3.8xlarge.

--- get_spot_prices.sh
+++ get_spot_prices.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 now_time=$(date -u "+%Y-%m-%dT%H:%M:%S")
-start_time=$(date -u -v-1d "+%Y-%m-%dT%H:%M:%S")
+start_time=$(date -u -v-12H "+%Y-%m-%dT%H:%M:%S")
 
 #echo $now_time
 #echo $start_time
+type=g3.8xlarge
+filename=g38_prices.tsv
 
+aws ec2 describe-spot-price-history --profile us --instance-types $type --product-descriptions Linux/UNIX --start-time $start_time > $filename
 
-aws ec2 describe-spot-price-history --profile us --instance-types g3.8xlarge --start-time $start_time
+python plot_timeseries.py $filename
--- plot_timeseries.py
+++ plot_timeseries.py
@@ -5,10 +5,9 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 import datetime
+import matplotlib.dates as mdates
 
-import plotly.plotly as py
-import plotly.tools as tls
-
+img_name="prices.pdf"
 
 if len(sys.argv) < 1:
     print "Need TSV file"
@@ -16,18 +15,33 @@ if len(sys.argv) < 1:
 
 file = sys.argv[1]
 print "Read from",file
-data = np.loadtxt(file,delimiter="\t",usecols=(5,4),
-    dtype=[('col1','datetime64[s]'),('col2','f8')])
+data = np.loadtxt(file, delimiter="\t", usecols=(5,4,1), dtype=object,
+                   converters={5: lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"),
+                      			4: np.float})
 
-print type(data)        #- numpy.ndarray
-print type(data[1][0])  #- numpy.datatime64
-x = data[0]
-y = data[1]
-print x
-print y
+z = data[:,2]
+colors=["r-","g-","b-","y-"]
+regions = np.unique(z)
+color_counter=0
+plt.close('all')
+plt.interactive(False)
+fig, ax = plt.subplots(1)
+fig.autofmt_xdate()
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%dT%H:%M'))
 
-plt.plot(data)
-plt.tight_layout()
-
-plt.savefig("foo.png")
-
+now = datetime.datetime.now()
+now = mdates.date2num(now)
+for region in regions:
+    print region
+    subarray=data[data[:,2] == region]
+    #print subarray
+    x = mdates.date2num(subarray[:,0])
+    x = np.append(now, x)
+    y = subarray[:,1]
+    y =np.append(y[0],y)
+    print y[0]
+    ax.plot_date(x,y,fmt=colors[color_counter],
+                  drawstyle="steps-pre",linewidth=0.5)
+    color_counter+=1
+ax.grid() 
+plt.savefig(img_name)
