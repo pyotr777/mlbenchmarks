2a321a1cb16b54977b73bda0477ca6d5419e4304
Peter Bryzgalov <peterbryz@yahoo.com>
2017-12-03 21:03:52 +0900

    Streamlined getting spot prices

--- get_spot_prices.sh
+++ get_spot_prices.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+# Get spot prices for US profile for last 12 hours.
+# Instance type can be set as CLI argument. Default is g3.8xlarge.
 
+type=g3.8xlarge
+
+if [[ -n "$1" ]];then
+   type="$1"
+fi
+
+filename="${type}_prices.tsv"
+graph="${type}_prices.pdf"
 now_time=$(date -u "+%Y-%m-%dT%H:%M:%S")
 start_time=$(date -u -v-12H "+%Y-%m-%dT%H:%M:%S")
 
 #echo $now_time
 #echo $start_time
-type=g3.8xlarge
-filename=g38_prices.tsv
-
+echo "Spot prices for $type in US profile for last 12 hours."
 aws ec2 describe-spot-price-history --profile us --instance-types $type --product-descriptions Linux/UNIX --start-time $start_time > $filename
 
-python plot_timeseries.py $filename
+python plot_timeseries.py $filename $graph $type
+
+open $graph
--- plot_timeseries.py
+++ plot_timeseries.py
@@ -13,21 +13,29 @@ if len(sys.argv) < 1:
     print "Need TSV file"
     exit(1)
 
-file = sys.argv[1]
-print "Read from",file
-data = np.loadtxt(file, delimiter="\t", usecols=(5,4,1), dtype=object,
+tsv_file = sys.argv[1]
+
+if len(sys.argv) > 1:
+    img_name=sys.argv[2]
+
+if len(sys.argv) > 2:
+    inst_type=sys.argv[3]
+else:
+    inst_type=tsv_file
+
+print "Read from",tsv_file
+data = np.loadtxt(tsv_file, delimiter="\t", usecols=(5,4,1), dtype=object,
                    converters={5: lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S.%fZ"),
                       			4: np.float})
 
 z = data[:,2]
-colors=["r-","g-","b-","y-"]
+colors=["#ff2200","#dd9900","#ccdd11","#66dd11","#00cc88","#0099cc"]
 regions = np.unique(z)
 color_counter=0
 plt.close('all')
 plt.interactive(False)
 fig, ax = plt.subplots(1)
-fig.autofmt_xdate()
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%dT%H:%M'))
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %H:%M'))
 
 now = datetime.datetime.now()
 now = mdates.date2num(now)
@@ -38,10 +46,30 @@ for region in regions:
     x = mdates.date2num(subarray[:,0])
     x = np.append(now, x)
     y = subarray[:,1]
-    y =np.append(y[0],y)
     print y[0]
+    y =np.append(y[0],y)
     ax.plot_date(x,y,fmt=colors[color_counter],
-                  drawstyle="steps-pre",linewidth=0.5)
+                  drawstyle="steps-pre",label=region,linewidth=0.5)
     color_counter+=1
-ax.grid() 
+    
+    if color_counter >= len(colors):
+        color_counter = 0
+
+mn = np.floor(min(data[:,1]))
+mx = np.ceil(max(data[:,1]))
+maxticks = 40
+arr = np.arange(mn, mx, 0.05)
+if len(arr) > maxticks:
+    arr = np.arange(mn, mx, 0.1)
+if len(arr) > maxticks:
+    arr = np.arange(mn, mx, 0.2)
+
+plt.yticks(arr)
+ax.grid(color="#ccddee",linewidth=0.3,linestyle="dotted") 
+ax.tick_params(axis="x", labelsize=5, rotation=60)
+ax.tick_params(axis="y",labelsize=5)
+#ax.yaxis.set_label_position("right")
+ax.yaxis.tick_right()
+ax.legend()
+plt.title(inst_type)
 plt.savefig(img_name)
