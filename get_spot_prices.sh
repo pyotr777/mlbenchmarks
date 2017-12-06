695ce4485cfa06e0c321ee94c529c023066157c2
Peter Bryzgalov <peterbryz@yahoo.com>
2017-12-06 20:06:05 +0900

    More graph for CUDA sample and HPCG profiling. Script to install TF HP without nvidia-docker.

--- get_spot_prices.sh
+++ get_spot_prices.sh
@@ -3,20 +3,25 @@
 # Instance type can be set as CLI argument. Default is g3.8xlarge.
 
 type=g3.8xlarge
+profile="us"
 
 if [[ -n "$1" ]];then
    type="$1"
+fi
+
+if [[ -n "$2" ]]; then
+	profile="$2"
 fi
 
 filename="${type}_prices.tsv"
 graph="${type}_prices.pdf"
 now_time=$(date -u "+%Y-%m-%dT%H:%M:%S")
 start_time=$(date -u -v-12H "+%Y-%m-%dT%H:%M:%S")
-
-#echo $now_time
-#echo $start_time
-echo "Spot prices for $type in US profile for last 12 hours."
-aws ec2 describe-spot-price-history --profile us --instance-types $type --product-descriptions Linux/UNIX --start-time $start_time > $filename
+start_time_local=$(date -v-12H "+%Y-%m-%dT%H:%M:%S")
+echo "$start_time_local "
+echo "Spot prices for $type in $profile profile for last 12 hours."
+set -x
+aws ec2 describe-spot-price-history --profile $profile --instance-types $type --product-descriptions Linux/UNIX --start-time $start_time > $filename
 
 python plot_timeseries.py $filename $graph $type
 
