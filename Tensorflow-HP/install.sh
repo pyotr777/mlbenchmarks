93c0513bb564d2ee241253189db780ce65ba9c7e
Peter Bryzgalov <peterbryz@yahoo.com>
2017-12-07 13:21:57 +0900

    install.sh - install on remote machine, executed on local, install_ubunutu.sh - executed on remote machine.

--- Tensorflow-HP/install.sh
+++ Tensorflow-HP/install.sh
@@ -1,70 +1,15 @@
 #!/bin/bash
 
-# Install required packages to run Tensorflow and TF HP benchmark.
-# Execute on remote machine whete TF will be installed.
-set -x
+# Install TF HP benchmark on remote machine
 
-sudo apt-get update && sudo apt-get install -y git libcupti-dev python-pip python-dev clang
-
-# Install CUDA 8
-if [[ ! -f cuda-repo-ubuntu1604_8.0.61-1_amd64.deb ]]; then
-	wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-fi
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo apt-get update
-sudo apt-get install -y cuda-8-0
-
-# Use CUDA
-export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
-export PATH="$PATH:/usr/local/cuda/bin"
-
-
-# Install TF
-pip install -U pip
-pip install --user tf-nightly-gpu
-
-INSTALL_FROM_SOURCE=""
-
-if [[ -n $INSTALL_FROM_SOURCE ]]; then
-
-
-	# Install TF from source
-
-
-	# Install cudNN
-	sudo dpkg -i libcudnn7_7.0.4.31-1+cuda9.0_amd64.deb
-	sudo dpkg -i libcudnn7-dev_7.0.4.31-1+cuda9.0_amd64.deb
-
-	# Install Bazel
-	sudo apt-get install -y openjdk-8-jdk
-	echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-	curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
-	sudo apt-get update && sudo apt-get install -y bazel
-	sudo apt-get upgrade -y bazel
-
-	sudo apt-get install -y python-numpy python-wheel
-
-
-
-	# Get CUDA Capability number
-	cuda-install-samples-9.0.sh .
-	cd NVIDIA_CUDA-9.0_Samples/1_Utilities/deviceQuery
-	make
-	./deviceQuery | grep -i capability
-	cd -
-	git clone https://github.com/tensorflow/tensorflow
-	cd tensorflow
-	./configure
-	bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
-	bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-	ls -l /tmp/tensorflow_pkg/
-	pip install --user /tmp/tensorflow_pkg/tensorflow-1.4.0-py2-none-any.whl
+if [[ $# -lt 1 ]]; then
+    echo "Need remote address"
+    exit 1
 fi
 
-# TODO:
-# NEED CUDNN 6
+remote="$1"
 
-# Install TF HP benchmark
-if [[ ! -d benchmarks || ! -f benchmarks/scripts/tf_cnn_benchmarks/tf_cnn_benchmarks.py ]]; then
-	git clone https://github.com/tensorflow/benchmarks.git
-fi
+scp _install.sh libcudnn6* $remote:
+ssh $remote ./install_ubuntu.sh
+
+echo "Installation script finished."
