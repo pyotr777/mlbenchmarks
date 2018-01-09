25b42de3620a12b8d4f7c9d1627e343329224fe3
Peter Bryzgalov <peterbryz@yahoo.com>
2018-01-09 16:39:02 +0900

    plot_traces.py plots line graphs for nvprof and smi data

--- plot_traces.py
+++ plot_traces.py
@@ -7,127 +7,27 @@ import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import csv
+import matplotlib.dates as dates
 import os.path
 import datetime
 from cycler import cycler
+import pandas as pd
+import matplotlib.ticker as ticker
+import sys
 
-print "v.0.58"
-
-min_duration = 0.001
-maxrows = 500000
-subplots = 6
-
-# Class object for information about series (E.g. "CUDA memcpy HtoD Pageble Device")
-# Name and index are defined on an instance creation (in __init__ function),
-# timestamp and value are filled later.
-class Series:
-    name = ""
-    timestamps = []
-    durations = []
-    values = []
-    index = 0   # column index
-    subplot = 1 # Subplot number
-
-    def __init__(self, name, index = 0):
-        self.name = name
-        self.index = index
-        self.values = []
-        self.timestamps = []
-        self.durations = []
-        subplot = 1
-
-    def fill(self,timestamp, duration, value):
-        self.timestamps.append(timestamp)
-        if duration < min_duration:
-            duration = min_duration
-        self.durations.append(duration)
-        self.values.append(value)
-
-    def printme(self):
-        print "name=",self.name,"length",len(self.timestamps),"plot",self.subplot
-        # for i, val in enumerate(self.timestamps):
-        #     print self.timestamps[i],self.values[i]
+print "v.0.102"
 
 
-# Returns existing array member if name exists,
-# or a new instance otherwise.
-def getSeriesByName(series_arr, name, index):
-    for series in series_arr:
-        if series.name == name:
-            return series
-    #print "Add new series ",name
-    series_arr.append(Series(name, index))
-    return series_arr[-1]
+trace_dir1 = "Tensorflow-HP" 
+trace_dir2 = "HPCG"
+filename1 = "nvprof-trace-tfhp.csv"
+filename2 = "nvprof-trace-hpcg.csv"
+filename_smi_tfhp = "nvidia-smi-tfhp.csv"
+filename_smi_hpcg = "nvidia-smi-hpcg.csv"
 
+maxrows = None
 
-# Returns an array of Series class instances,
-# where for existing names existing instances are used,
-# and for new names new instances are created.
-def getSeriesArray(series_arr, names, indexes, subplots):
-    new_series = []
-    for i, val in enumerate(names):
-        series = getSeriesByName(series_arr,names[i], indexes[i])
-        series.subplot = subplots[i]
-        new_series.append(series)
-    return new_series
-
-# Returns an array of Series class instances
-# from series_arr if instance with the same name exitst,
-# new instance otherwise.
-# Uses global variables for column indexes.
-def getCudaSeries(series_arr, line):
-    base_name = line[name_field_index]+" "+line[src_field_index] +line[dst_field_index]
-    names = [ base_name + " Throughput(GB/s)"]
-    col_indexes = [throughput_field_index]
-    subplots = [1]
-    if line[name_field_index].find("DtoD") > 0 or line[name_field_index].find("memset") > 0:
-        subplots = [2]
-    return getSeriesArray(series_arr,names,col_indexes, subplots)
-
-
-# Returns an array of Series class instances
-# from series_arr if instance with the same name exitst,
-# new instance otherwise.
-# Uses global variables for column indexes.
-def getKernelSeries(series_arr, line):
-    base_name = "Stream" + line[stream_index] # Put all kernels in one series
-    names = [base_name + " Static SMem(KB)", base_name + " Dynamic SMem(B)"]
-    col_indexes = [SSMem_field_index, DSMem_field_index]
-    subplots = [3,3]
-    return getSeriesArray(series_arr,names,col_indexes, subplots)
-
-# Save data from CSV file record
-# to Series class instance.
-def fillSeries(series, line):
-    # print series.name,line[time_field_index],line[series.index]
-    series.fill(line[time_field_index],float(line[duration_field_index])/1000,line[series.index])
-
-
-trace_dir = "Tensorflow-HP"
-filename1 = "nvidia-smi-tfhp.csv"
-filename2 = "nvprof-trace-tfhp.csv"
-# trace_dir = "HPCG"
-# filename1 = "nvidia-smi-hpcg.csv"
-# filename2 = "nvprof-trace-hpcg.csv"
-
-file1 = os.path.join(trace_dir,filename1)
-file2 = os.path.join(trace_dir,filename2)
-
-print "Reading",file1,file2
-
-
-
-series_arr1 = [] # Array of Series class instances
-
-
-# Reading nvprof trace
-filename = file2
-ignore_pattern = re.compile("^[a-zA-z]+")
-cuda_pattern = re.compile("\[CUDA .*\]")
-# Column indexes
-# nvprof trace file
-name_field_index = 18
+# nvidia-smi trace format
 time_field_index = 0
 duration_field_index = 1
 SSMem_field_index = 9
@@ -138,96 +38,151 @@ src_field_index = 13
 dst_field_index = 14
 context_index = 16
 stream_index = 17
+name_field_index = 18
+
+file1 = os.path.join(trace_dir1,filename1)
+file2 = os.path.join(trace_dir2,filename2)
+
+print "Reading",file1,file2
+
+columns = [time_field_index, duration_field_index,
+           SSMem_field_index, DSMem_field_index,
+           size_field_index, throughput_field_index,
+           src_field_index, dst_field_index,
+           context_index, stream_index,
+           name_field_index]
 
 
-rowcounter = 1
-
-with open(filename, "rb") as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        if rowcounter > maxrows:
-            # print len(series_arr1)
-            for series in series_arr1:
-                series.printme()
-            break
-        if len(line) > 3:
-            if ignore_pattern.search(line[0]) is None:
-                if cuda_pattern.search(line[name_field_index]) is not None:
-                    # print line
-                    # Get a new or existing instance of class Series
-                    series_arr = getCudaSeries(series_arr1,line)
-                else:
-                    series_arr = getKernelSeries(series_arr1,line)
-                # Fill series instance with corresponding fields of the CSV file line
-                for series in series_arr:
-                    fillSeries(series, line)
-                    #print "Filled series",series.name
-                    #series.printme()
-        rowcounter += 1
-    csvfile.close()
-
-print "nvprof array length", len(series_arr1)
-# print series_arr1
-
-img_name = trace_dir+".pdf"
-
-plt.interactive(False)
-#plt.style.use('ggplot')
-# Set figure width to 12 and height to 9
-fig_size = plt.rcParams["figure.figsize"]
-fig_size[0] = 15
-fig_size[1] = 10
-plt.rcParams["figure.figsize"] = fig_size
-
-fig, axarr = plt.subplots(subplots,sharex=True)
-#fig.subplots_adjust(hspace=0)
-axarr[0].set_title("nvprof "+trace_dir)
-
-# colors=[["#f9ea62","#ce8900","#ffad74","#ff9015","#ff5000","#95361d"],
-#     ["#9fe0b6","#00ae42","#cde67e","#20d2c4","#00aac7","#0055bb"]]
-#colors=["#f9ea62","#ce8900","#ffad74","#ff9015","#ff5000","#95361d"]
-colors=["#39a6f4","#fdb94c","#49dd4c","#6bd5de","#f78ae6","#ff5000"]
-art = []
-
-rightax = axarr[2].twinx()
-rightax.set_prop_cycle(cycler('color',colors))
-
-for series in series_arr1:
-    x = np.array(series.timestamps, dtype = float)
-    y = np.array(series.values, dtype = float)
-    w = np.array(series.durations, dtype = float)
-    #print series.name,x,y, w
-    #axarr.scatter(x,y,s=0.5,alpha=0.5,label=series.name)
-    #axarr.plot(x,y,linewidth=0.5,alpha=0.5,label=series.name)
-    if series.name.find("Dynamic") > 0:
-        axis = rightax
-    else:
-        axis = axarr[series.subplot-1]
-    axis.bar(x,y,w,alpha=0.9,label=series.name)
+# Save current plot to a file
+def saveFig(img_name):
+    print "saveing to "+ img_name
+    plt.savefig(img_name, bbox_inches='tight')
 
 
+# Convert unique values in column "FullName" to new columns
+def mergeColumnNames(df_org):
+    df = df_org.pivot(index = 'Start', columns = 'FullName', 
+                         values = 'Throughput')
+    df = df.fillna(0)  # Fill empty cells with 0-s
+    return df
+
+
+# Add max values to column names
+def appendMaxValues2ColumnNames(df,series):
+    cols = len(df.columns)
+    col_names = []
+    for i in range(0,cols):
+        col_names.append(series+df.columns[i]+" " +'{:.3f}'.format(df.iloc[:,i].max()))
+    df.columns = col_names
+    return df
+
+
+# Plot BOX plot of DataFrame columns
+# and save to an image file
+def boxPlotDF( df, imgname):
+    # To remove zero values need to split DataFrame into Series (they will have different lengths)
+    # Convert columns to Series and add to a list
+    # Create list of column names
+    x_arr = []
+    names = []
+    for column in df: 
+        x = df[column]
+        x = x[x != 0]
+        print '{:46.44} {:8.8} '.format(column, x.shape),
+        arr = x.values
+        x_arr.append(arr)
+        names.append(column)
+        print '{:3d}'.format(len(x_arr))
+
+    # BOX plot of memcpy operations
+    plt.interactive(False)
+    plt.rcParams['figure.figsize'] = 12,8
+    fig, axis = plt.subplots(1)
+    axis.boxplot(x_arr, 0, '', labels = names)
+    plt.xticks(rotation=90)
+    #ax = plt.gca()
+    #ax.set_yscale("log")
+    axis.yaxis.grid(color="#e0e0e0", linestyle=":",linewidth=0.5)
+    saveFig(imgname)
+
+
+# Reading nvprof trace
+print file1
+df_tf = pd.read_csv(file1, header = 0, usecols = columns, 
+                 skiprows=[0,1,2,4], nrows = maxrows)
+print df_tf.shape
+print file2
+df_hpcg = pd.read_csv(file2, header = 0, usecols = columns, 
+                 skiprows=[0,1,2,4], nrows = maxrows)
+print df_hpcg.shape
+
+# Select columns with memory operations
+df_tf_cuda = df_tf.loc[df_tf['Name'].str.contains('\[CUDA')]
+df_hpcg_cuda = df_hpcg.loc[df_hpcg['Name'].str.contains('\[CUDA')]
+
+df_tf_cuda['FullName'] = df_tf_cuda['Name'] + " " + df_tf_cuda['SrcMemType']+df_tf_cuda['DstMemType'].fillna("")
+df_hpcg_cuda['FullName'] = df_hpcg_cuda['Name'] + " " + df_hpcg_cuda['SrcMemType']+df_hpcg_cuda['DstMemType'].fillna("")
+
+df_tf_throughput = mergeColumnNames(df_tf_cuda)
+df_hpcg_throughput = mergeColumnNames(df_hpcg_cuda)
+
+df_tf_throughput = appendMaxValues2ColumnNames(df_tf_throughput,"TF")
+df_hpcg_throughput = appendMaxValues2ColumnNames(df_hpcg_throughput,"HPCG")
+
+plt.rcParams['figure.figsize'] = 12,8
+
+# Concatenate columns of two DFs into one DF
+df_full = pd.concat([df_tf_throughput,df_hpcg_throughput], axis = 1).fillna(0)
+
+# Remove "memset" columns
+df_memcpy = df_full.filter(regex=("^((?!memset).)*$"))
+df_DH = df_memcpy.filter(regex=(".*(HtoD|DtoH).*"))
+df_DD = df_memcpy.filter(regex=(".*DtoD.*"))
+
+boxPlotDF(df_DD, "memcpy_DD_box.pdf")
+boxPlotDF(df_DH, "memcpy_DHHD_box.pdf")
+
+
+# Plot memory operation profiles
+df_DD_TF = df_DD.filter(regex=("^TF.*"))
+df_DD_HPCG = df_DD.filter(regex=("^HPCG.*"))
+
+df_DH_TF = df_DH.filter(regex=("^TF.*"))
+df_DH_HPCG = df_DH.filter(regex=("^HPCG.*"))
+
+plt.rcParams['figure.figsize'] = 14,12
+fig, axarr = plt.subplots(4)
+df_DD_TF.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.7,ax = axarr[0])
+df_DD_HPCG.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.7,ax = axarr[1])
+df_DH_TF.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.7,ax = axarr[2])
+df_DH_HPCG.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.7,ax = axarr[3])
 for axis in axarr:
     axis.legend()
-    axis.xaxis.grid(color="#e0e0e0", linestyle="--",linewidth=0.5)
+    axis.xaxis.grid(color="#e0e0e0", linestyle=":",linewidth=0.5)
     axis.xaxis.set_major_locator(plt.MaxNLocator(24))
+saveFig("memcpy_graph.pdf")
 
-right_legend = rightax.legend(loc = 'upper left', bbox_to_anchor=(1.02,1))
-art.append(right_legend)
+#sys.exit()
 
-# Creates an array of Series instances
-def parseSeriesNames(line):
-    skip_columns = 2 # Skip first 2 columns
-    columns = line[skip_columns:]
-    print columns
-    series = [None for i in range(len(columns))]
-    for i,title in enumerate(columns):
-        series[i] = Series(title,i+skip_columns)
-    return series
 
-start = 0
+
+# SMI data
+
+filename1 = os.path.join(trace_dir1, filename_smi_tfhp)
+filename2 = os.path.join(trace_dir2, filename_smi_hpcg)
+print "Reading",filename1,filename2
+
+# Reading nvidia-smi trace
+# File must be of the following format:
+#   first line - column titles,
+#   first column - timestamps.
+smi_tfhp = pd.read_csv(filename1)
+smi_hpcg = pd.read_csv(filename2)
+
+
 # Parse date from readable format to seconds
-def parseTime(date_time):
-    global start
+def parseTime(date_time, start = 0):
+    #global start
     dt_obj = datetime.datetime.strptime(date_time, "%Y/%m/%d %H:%M:%S.%f")
     seconds = 0
     if start == 0:
@@ -237,6 +192,8 @@ def parseTime(date_time):
     return seconds
 
 pat = re.compile("[0-9\.]+")
+
+
 # Extract float number from a string
 def parseFloat(str):
     global pat
@@ -247,49 +204,55 @@ def parseFloat(str):
     return None
 
 
-# Reading nvidia-smi trace
-# File must be of the following format:
-#   first line - column titles,
-#   first column - timestamps.
 
-filename = file1
-series_arr2 = []
-time_field_index = 0
+smi_tfhp.index = pd.to_datetime(smi_tfhp['timestamp'])
+start = smi_tfhp.index[0]
+smi_tfhp["sec"] = smi_tfhp["timestamp"].apply(parseTime, args = (start,))
 
-with open(filename, "rb") as csvfile:
-    reader = csv.reader(csvfile)
-    titles = reader.next()
-    series_arr2 = parseSeriesNames(titles) # array of instances of class Series
-    rowcounter=1
-    for line in reader:
-        timestamp = parseTime(line[time_field_index])
-        for series in series_arr2:
-            series.timestamps.append(timestamp)
-            series.values.append(parseFloat(line[series.index]))
-            series.subplot = 4
-            if series.name.find("utilization") >= 0:
-                series.subplot = 5
-            elif series.name.find("clocks") >= 0:
-                series.subplot = 6
-        if rowcounter > maxrows:
-            break
-        rowcounter += 1
+smi_tfhp = smi_tfhp.set_index(["sec"])
+smi_tfhp = smi_tfhp.drop(columns = ['timestamp', ' name'])
 
-    csvfile.close()
+smi_tfhp = smi_tfhp.applymap(parseFloat)
+smi_tfhp_MB = smi_tfhp.filter(regex=(".*\[MiB.*"))
+smi_tfhp_pr = smi_tfhp.filter(regex=(".*\[\%.*"))
+smi_tfhp_Hz = smi_tfhp.filter(regex=(".*\[MHz.*"))
 
-print "nvidia-smi array length", len(series_arr2)
-for series in series_arr2:
-    x = np.array(series.timestamps, dtype = float)
-    y = np.array(series.values, dtype = float)
-    axis = axarr[series.subplot-1]
-    axis.plot(x,y,alpha=0.9,label=series.name)
 
-axarr[3].set_title("nvidia-smi "+trace_dir)
+smi_hpcg.index = pd.to_datetime(smi_hpcg['timestamp'])
+start = smi_hpcg.index[0]
+smi_hpcg["sec"] = smi_hpcg["timestamp"].apply(parseTime, args = (start,))
 
+smi_hpcg = smi_hpcg.set_index(["sec"])
+smi_hpcg = smi_hpcg.drop(columns = ['timestamp', ' name'])
+
+smi_hpcg = smi_hpcg.applymap(parseFloat)
+smi_hpcg_MB = smi_hpcg.filter(regex=(".*\[MiB.*"))
+smi_hpcg_pr = smi_hpcg.filter(regex=(".*\[\%.*"))
+smi_hpcg_Hz = smi_hpcg.filter(regex=(".*\[MHz.*"))
+
+
+plt.rcParams['figure.figsize'] = 12,12
+fig, axarr = plt.subplots(6)
+smi_tfhp_MB.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.9,ax = axarr[0])
+axarr[0].set_title("nvidia-smi TF")
+smi_hpcg_MB.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.9,ax = axarr[1])
+axarr[1].set_title("HPCG")
+smi_tfhp_pr.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.9,ax = axarr[2])
+axarr[2].set_title("TF")
+smi_hpcg_pr.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.9,ax = axarr[3])
+axarr[3].set_title("HPCG")
+smi_tfhp_Hz.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.9,ax = axarr[4])
+axarr[4].set_title("TF")
+smi_hpcg_Hz.plot(drawstyle="steps-post",linewidth=0.5,alpha=0.9,ax = axarr[5])
+axarr[5].set_title("HPCG")
 for axis in axarr:
     axis.legend()
+    axis.xaxis.grid(color="#e0e0e0", linestyle=":",linewidth=0.5)
+    axis.xaxis.set_major_locator(plt.MaxNLocator(24))
+    axis.yaxis.grid(color="#e0e0e0", linestyle=":",linewidth=0.5)
 
 
-plt.savefig(img_name, bbox_extra_artists=art, bbox_inches='tight')
+
+saveFig("nvidia_smi.pdf")
 
 
