c1b2f7917dbd910b9f931a2239b94c6ecee42080
Peter Bryzgalov <peterbryz@yahoo.com>
2018-01-16 18:39:31 +0900

    Modified Chainer installer script not to copy files that already been copied.

--- Chainer/install.sh
+++ Chainer/install.sh
@@ -14,13 +14,42 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-echo "Installing Chainer and Cifar100 benchmark on $1"
-FILES=("ubuntu_install.sh" "../CUDNN7/libcudnn7_7.0.4.31-1+cuda9.0_amd64.deb" "../CUDNN7/libcudnn7-dev_7.0.4.31-1+cuda9.0_amd64.deb" "run.sh" "../comb_profile.sh")
-for F in ${FILES[@]}; do
-	echo "Copying $F"
-	scp $F $1:
-done
+ADDRESS=$1
+shift
+
+INSTALLER="ubuntu_install_chainer.sh"
+
+function copy_files {
+	FILES=$1
+	ADDR=$2
+	REMOTE_FILES=$(ssh $ADDR ls -1 2>/dev/null)
+	echo "Files ${FILES[@]}"
+	echo "Copy to $ADDR"
+	for F in ${FILES[@]}; do
+		base=$(basename $F)
+		COPY="yes"
+		for RF in ${REMOTE_FILES}; do
+			if [[ "$base" == "$RF" ]]; then
+				echo "$base exists"
+				COPY=""
+				continue
+			fi
+		done
+		if [[ -n "$COPY" ]]; then
+			echo "Copying $F"
+			scp $F $ADDRESS:
+		fi
+	done
+}
+
+echo "Installing Chainer and Cifar100 benchmark on $ADDRESS"
+FILES=("$INSTALLER" "../CUDNN7/libcudnn7_7.0.4.31-1+cuda9.0_amd64.deb" "../CUDNN7/libcudnn7-dev_7.0.4.31-1+cuda9.0_amd64.deb" "run_cifar.sh" "../comb_profile.sh")
+
+#REMOTE_FILES=$(ssh $ADDRESS ls -1 2>/dev/null)
+#echo "Remote files: ${REMOTE_FILES[@]}"
+copy_files $FILES $ADDRESS
+
 
 set -ex
-ssh $1 ./ubuntu_install.sh
-echo "Installation finished. Login with ssh $1 and run ./run.sh"
+ssh $ADDRESS ./$INSTALLER
+echo "Installation finished. Login with ssh $ADDRESS and run ./run.sh"
