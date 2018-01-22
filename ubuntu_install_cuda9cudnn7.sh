ebc5f6f8fe9a0170f360a3c158ff3408ae1ee43c
Peter Bryzgalov <peterbryz@yahoo.com>
2018-01-22 21:50:00 +0900

    Added Chainer on V100 performance. Fixed installation files.

--- ubuntu_install_cuda9cudnn7.sh
+++ ubuntu_install_cuda9cudnn7.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#cuDNN="libcudnn7_7.0.4.31-1+cuda9.0_amd64.deb"
+cuDNN="libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb"
+cuDNNdev="libcudnn7-dev_7.0.5.15-1+cuda9.0_amd64.deb"
+
 # Install PIP
 sudo apt-get update && sudo apt-get install -y libcupti-dev python-pip python-dev
 
@@ -15,9 +19,9 @@ sudo apt-get update
 sudo apt-get install -y --allow-unauthenticated cuda
 
 # Install CUDNN
-sudo dpkg -i libcudnn7_7.0.4.31-1+cuda9.0_amd64.deb
+sudo dpkg -i "$cuDNN"
 # If not installed libcudnn7-dev_7.0.4.31-1+cuda9.0_amd64.deb Cupy complains "cuDNN is not enabled"
-sudo dpkg -i libcudnn7-dev_7.0.4.31-1+cuda9.0_amd64.deb
+sudo dpkg -i "$cuDNNdev"
 if [[ ! -a /usr/local/cuda/include/cudnn.h ]]; then
 	sudo ln -s /usr/include/cudnn.h /usr/local/cuda/include/cudnn.h
 	sudo ln -s /usr/lib/x86_64-linux-gnu/libcudnn.so /usr/local/cuda/lib64/libcudnn.so
