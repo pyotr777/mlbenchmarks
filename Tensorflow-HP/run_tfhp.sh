c995665acaa9f6971dda4fea650ca1b8e910a99f
Peter Bryzgalov <peterbryz@yahoo.com>
2018-01-25 19:42:59 +0900

    Bandwidth for K80. Short presentation. In TF runs script correct batch size and num_batches for benchmarking.

--- Tensorflow-HP/run_tfhp.sh
+++ Tensorflow-HP/run_tfhp.sh
@@ -2,4 +2,4 @@
 
 # This script will start TF HP benchmark.
 cd $HOME/benchmarks/scripts/tf_cnn_benchmarks/
-python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=32 --model=resnet50 --num_batches=3
+python tf_cnn_benchmarks.py --num_gpus=1 --batch_size=64 --model=resnet50 #--num_batches=3
