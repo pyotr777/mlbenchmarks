2b2aaff5bbe2cd001d45fdd1a401f2ef46f8d824
Peter Bryzgalov <peterbryz@yahoo.com>
2018-01-29 19:58:26 +0900

    Install specific version of cupy. Do not use cache.

--- Chainer/install_chainer.sh
+++ Chainer/install_chainer.sh
@@ -2,7 +2,7 @@
 
 # Install python pakcages
 pip install -U pip
-pip install -U --user cupy #chainer
+pip install -U --user cupy==4.0.0b2 --no-cache-dir #chainer
 
 # Clone Chainer
 git clone https://github.com/chainer/chainer
@@ -10,6 +10,6 @@ git clone https://github.com/chainer/chainer
 # Download CIFAR dataset and test Chainer
 cd chainer
 git checkout v4.0.0b2
-pip install --user .
+pip install --user . --no-cache-dir
 
 
