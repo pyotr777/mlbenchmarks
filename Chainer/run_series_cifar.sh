9b553cc5bb36747eb89da53a17399ee4d6368913
Peter Bryzgalov <peterbryz@yahoo.com>
2018-02-09 18:10:18 +0900

    More Chainer measurements, including some of Azure NC6.

--- Chainer/run_series_cifar.sh
+++ Chainer/run_series_cifar.sh
@@ -4,9 +4,9 @@
 
 
 batchsizes=(32 64 128 256 384 512 640 768 896 1024)
-
+EPOCHS=2
 
 for batchsize in "${batchsizes[@]}"; do
 	echo $batchsize
-    ./run_cifar.sh -b $batchsize -e 10 2>/dev/null
+    ./run_cifar.sh -b $batchsize -e $EPOCHS 2>/dev/null
 done
