81b4c5fdb67f423975791d1b14547178f18951ab
Peter Bryzgalov <peterbryz@yahoo.com>
2018-02-20 20:19:30 +0900

    Change run_cifar script to accept all CLI arguments and pass them to train_cifar.py.

--- Chainer/run_cifar.sh
+++ Chainer/run_cifar.sh
@@ -1,43 +1,4 @@
 #!/bin/bash
+# Run train_cifar sample on GPU with CIFAR100 dataset
 
-usage=$(cat <<USAGEBLOCK
-Usage:
-$0 [-n/--num_gpus <int>] [--batch_size <int>] [...]
-
-Options:
-	-b, --batch_size	Batch size
-	-e					Epochs to run
-	-h, --help			This help info.
-	--debug				Print debug info.
-USAGEBLOCK
-)
-
-# Defaults
-EPOCHS=2
-BATCH=256
-
-while test $# -gt 0; do
-	case "$1" in
-		-h | --help)
-			echo "$usage"
-			exit 0
-			;;
-		-b | --batch_size)
-			BATCH=$2;shift;
-			;;
-		-e )
-			EPOCHS=$2;shift;
-			;;
-		--)
-			shift
-			break;;
-		--*)
-			OTHER_OPTIONS="$OTHER_OPTIONS $1=$2";shift;
-			;;
-	esac
-	shift
-done
-
-
-cd chainer/examples/cifar
-python train_cifar.py -d cifar100 -g 0 -b $BATCH -e $EPOCHS
+python chainer/examples/cifar/train_cifar.py -d cifar100 -g 0 $@
