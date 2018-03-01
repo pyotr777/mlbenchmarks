cca5841580e331c2db9875f9fd4b1a9cd23d0fda
Peter Bryzgalov <peterbryz@yahoo.com>
2018-03-01 20:20:49 +0900

    Run series with various batch size and learning rates. Use deterministic script train_cifar_determ.py.

--- Chainer/run_series_cifar.sh
+++ Chainer/run_series_cifar.sh
@@ -2,11 +2,13 @@
 
 # Run Chainer CIFAR100 with different batch-sizes
 
-
-batchsizes=(32 48 64 80 128 256 384 512 640 1024)
+batchsizes=(32 48 64 80 128 256 384 512 640)
+learnrates=(0.15 0.1 0.05 0.025 0.01 0.005 0.001)
 EPOCHS=10
 
 for batchsize in "${batchsizes[@]}"; do
-	echo $batchsize
-    ./run_cifar.sh -b $batchsize -e $EPOCHS 2>/dev/null
+	for learnrate in "${learnrates[@]}"; do
+		echo "b $batchsize l $learnrate"
+	    ./run_cifar_determ.sh -b $batchsize -l $learnrate -e $EPOCHS #2>/dev/null
+	done
 done
