8026ce8b06d6a3cf94627e27415abd28c6d56fd1
Peter Bryzgalov <peterbryz@yahoo.com>
2018-03-29 17:48:16 +0900

    Modified script for running on mouse with 2 runs each with 400 randomly selected BS from range [64,900].

--- Chainer/cifar_scripts/run_multigpu_microseries.py
+++ Chainer/cifar_scripts/run_multigpu_microseries.py
@@ -10,11 +10,12 @@ import re
 import time
 import os
 import numpy as np
+import random
 
 # Returns True if GPU #i is not used.
 # Uses nvidia-smi command to monitor GPU SM usage.
 def GPUisFree(i):
-    command = "nvidia-smi dmon -c 3 -d 1 -i {} -s u".format(i)
+    command = "nvidia-smi dmon -c 2 -d 2 -i {} -s u".format(i)
     nvsmi_pattern = re.compile(r"^\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)")
     proc = subprocess.Popen(command.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1, shell=False)
     u = 0
@@ -44,7 +45,7 @@ def getNextFreeGPU(start=0):
             if GPUisFree(i):
                 return i
             print "busy"
-            time.sleep(1)
+            time.sleep(5)
 
 
 # Runs a task on specified GPU
@@ -59,22 +60,24 @@ def runTask(task,gpu):
     print pid
 
 
-gpus = 8
-batchsizes=np.arange(64,1025,1)
-learnrates=[0.15]
-epochs=1
+gpus = 1
+runs = 2
 tasks = []
-for batch in batchsizes:
-    for lr in learnrates:
-        logfile="logs/microseries/cifar_log_b{}_l{:.3f}.log".format(batch,lr)
-        if os.path.isfile(logfile):
-            print "file",logfile,"exists."
-            continue
-        f = open(logfile,"w+")
-        f.write("b{} l{}\n".format(batch,lr))
-        f.close()
-        task = {"comm":"python chainer/examples/cifar/train_cifar.py -d cifar100 -e {} -b {} -l {} ".format(epochs,batch,lr),"logfile":logfile}
-        tasks.append(task)
+for run in range(runs):
+    batchsizes = random.sample(np.arange(64,900),400)
+    learnrates=[0.15]
+    epochs=1
+    for batch in batchsizes:
+        for lr in learnrates:
+            logfile="logs/microseries/cifar_log_b{}_l{:.3f}_{:04d}.log".format(batch,lr,run)
+            if os.path.isfile(logfile):
+                print "file",logfile,"exists."
+                continue
+            f = open(logfile,"w+")
+            f.write("b{} l{}\n".format(batch,lr))
+            f.close()
+            task = {"comm":"python chainer/examples/cifar/train_cifar.py -d cifar100 -e {} -b {} -l {} ".format(epochs,batch,lr),"logfile":logfile}
+            tasks.append(task)
 
 print "Have",len(tasks),"tasks"
 gpu = -1
@@ -82,6 +85,6 @@ for i in range(0,len(tasks)):
     #print "Preapare",tasks[i]["comm"],">",tasks[i]["logfile"]
     gpu = getNextFreeGPU(gpu+1)
     runTask(tasks[i],gpu)
-    time.sleep(3)
+    time.sleep(15)
 
 
