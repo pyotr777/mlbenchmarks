682f3f07f56de537d28a06bfa8e312af525f9683
Peter Bryzgalov <peterbryz@yahoo.com>
2018-04-04 21:06:44 +0900

    Image caption series on DL. run_container.sh mount host directory .torch into container to prevent model download for every container.

--- image-caption/run_container.sh
+++ image-caption/run_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Start docker container
-comm="nvidia-docker run -t --rm --shm-size 8G -v $(pwd):/workspace/image_caption -w /workspace/image_caption image-caption /workspace/image_caption/init_container.sh && $@"
+comm="nvidia-docker run -t --rm --shm-size 8G -v $(pwd):/workspace/image_caption -v $(pwd)/.torch:/root/.torch -w /workspace/image_caption image-caption /workspace/image_caption/init_container.sh $@"
 echo "NV_GPU=$NV_GPU"
 echo "Runnning command $comm"
-cont=$(nvidia-docker run -t --rm --shm-size 8G -v $(pwd):/workspace/image_caption -w /workspace/image_caption image-caption /workspace/image_caption/init_container.sh $@)
-echo "Container $cont stopped"
-docker logs $cont
+$comm 2>&1
+echo "Container stopped."
+#docker logs $cont
